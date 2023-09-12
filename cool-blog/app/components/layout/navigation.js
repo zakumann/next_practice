4f06c2eda8056dc4d904937d4b6e63496d38ca3d
zakumann <samuel742able@gmail.com>
2023-09-13 00:25:38 +0900

    updating the all-posts page

--- cool-blog/app/components/layout/navigation.js
+++ cool-blog/app/components/layout/navigation.js
@@ -8,6 +8,7 @@ export default function Navigation(){
             <nav>
                 <ul>
                     <li><Link href="/posts" className={styles["nav-link"]}>Posts</Link></li>
+                    <li><Link href="/admin/create-post" className={styles["nav-link"]}>Create Post</Link></li>
                     <li><Link href="/login" className={styles["nav-link"]}>Sign In</Link></li>
                 </ul>
             </nav>
