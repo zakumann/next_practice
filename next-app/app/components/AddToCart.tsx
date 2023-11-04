6f0d659ad01b9934e9591514ecb6b5f57fc15548
zakumann <samuel742able@gmail.com>
2023-11-04 22:29:57 +0900

    clone coding

---
+++ next-app/app/components/AddToCart.tsx
@@ -0,0 +1,10 @@
+'use client';
+import React from 'react'
+
+const AddToCart = () => {
+  return (
+    <button className='btn btn-primary' onClick={() => console.log('Click')}>Add to Cart</button>
+  )
+}
+
+export default AddToCart
